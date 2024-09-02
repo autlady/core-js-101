19ba7594ebecb2c3da5c0d6019df3366625a51df
Julia Grigoreva <julia_panina@mail.ru>
2024-09-02 18:23:16 +0300

    feat: func unbracketTag

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -149,8 +149,10 @@ function removeFirstOccurrences(str, value) {
  *   '<span>' => 'span'
  *   '<a>' => 'a'
  */
-function unbracketTag(/* str */) {
-  throw new Error('Not implemented');
+function unbracketTag(str) {
+  return str.slice(1, -1);
+
+  // throw new Error('Not implemented');
 }
 
 
