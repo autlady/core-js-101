89a7e2507d9abdf3a5473bdc5e1cf95ead4b32e6
Julia Grigoreva <julia_panina@mail.ru>
2024-09-15 19:58:32 +0300

    feat: getUpperCaseStrings and getStringsLength

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -122,8 +122,9 @@ function removeFalsyValues(arr) {
  *    => [ 'PERMANENT-INTERNSHIP', 'GLUTINOUS-SHRIEK', 'MULTIPLICATIVE-ELEVATION' ],
  *    [ 'a', 'b', 'c', 'd', 'e', 'f', 'g' ]  => [ 'A', 'B', 'C', 'D', 'E', 'F', 'G' ]
  */
-function getUpperCaseStrings(/* arr */) {
-  throw new Error('Not implemented');
+function getUpperCaseStrings(arr) {
+  return arr.map((item) => item.toUpperCase());
+  // throw new Error('Not implemented');
 }
 
 
@@ -137,8 +138,9 @@ function getUpperCaseStrings(/* arr */) {
  *    [ '', 'a', 'bc', 'def', 'ghij' ]  => [ 0, 1, 2, 3, 4 ]
  *    [ 'angular', 'react', 'ember' ] => [ 7, 5, 5 ]
  */
-function getStringsLength(/* arr */) {
-  throw new Error('Not implemented');
+function getStringsLength(arr) {
+  return arr.map((item) => item.length);
+  // throw new Error('Not implemented');
 }
 
 /**
