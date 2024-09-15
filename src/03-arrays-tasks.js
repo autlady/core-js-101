0cce66c2f548ed02fdf3d79347811e7ba00cbdb9
Julia Grigoreva <julia_panina@mail.ru>
2024-09-15 20:29:58 +0300

    feat: func propagateItemsByPositionIndex  get3TopItems

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -267,8 +267,9 @@ function getMovingSum(arr) {
  * [ 'a', 'b', 'c' , null ]  => [ "b", null ]
  * [ "a" ] => []
  */
-function getSecondItems(/* arr */) {
-  throw new Error('Not implemented');
+function getSecondItems(arr) {
+  return arr.filter((item, index) => index % 2 === 1);
+  // throw new Error('Not implemented');
 }
 
 
@@ -286,8 +287,13 @@ function getSecondItems(/* arr */) {
  *  [ 'a', 'b', 'c', null ] => [ 'a', 'b','b', 'c','c','c',  null,null,null,null ]
  *  [ 1,2,3,4,5 ] => [ 1, 2,2, 3,3,3, 4,4,4,4, 5,5,5,5,5 ]
  */
-function propagateItemsByPositionIndex(/* arr */) {
-  throw new Error('Not implemented');
+function propagateItemsByPositionIndex(arr) {
+  return arr.flatMap((item, index) => {
+    const count = index + 1;
+    return new Array(count).fill(item);
+  });
+
+  // throw new Error('Not implemented');
 }
 
 
@@ -304,8 +310,9 @@ function propagateItemsByPositionIndex(/* arr */) {
  *   [ 1,2,3,4,5,6,7,8,9,10 ] => [ 10, 9, 8 ]
  *   [ 10, 10, 10, 10 ] => [ 10, 10, 10 ]
  */
-function get3TopItems(/* arr */) {
-  throw new Error('Not implemented');
+function get3TopItems(arr) {
+  return arr.sort((a, b) => b - a).slice(0, 3);
+  // throw new Error('Not implemented');
 }
 
 
