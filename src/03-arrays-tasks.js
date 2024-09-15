8887c539b78e7ce0045861ad878bd62536ea7162
Julia Grigoreva <julia_panina@mail.ru>
2024-09-15 20:07:15 +0300

    feat: func getTail and toCsvText

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -185,8 +185,9 @@ function getHead(arr, n) {
  *    [ 1, 3, 4, 5 ], 2  => [ 4, 5 ]
  *    [ 'a', 'b', 'c', 'd'], 3  => [ 'b', 'c', 'd' ]
  */
-function getTail(/* arr, n */) {
-  throw new Error('Not implemented');
+function getTail(arr, n) {
+  return arr.splice(-n);
+  // throw new Error('Not implemented');
 }
 
 
@@ -210,8 +211,9 @@ function getTail(/* arr, n */) {
  *    +'20,21,22,23,24\n'
  *    +'30,31,32,33,34'
  */
-function toCsvText(/* arr */) {
-  throw new Error('Not implemented');
+function toCsvText(arr) {
+  return arr.map((row) => row.join(',')).join('\n');
+  // throw new Error('Not implemented');
 }
 
 /**
