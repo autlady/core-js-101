6ef1b16ba54cf54e25b9fea661761852c49101c2
Julia Grigoreva <julia_panina@mail.ru>
2024-09-19 15:01:17 +0300

    feat: getSumBetweenNumbers and isTriangle

--- src/04-conditions-n-loops-tasks.js
+++ src/04-conditions-n-loops-tasks.js
@@ -65,8 +65,9 @@ function getFactorial(n) {
  *   5,10  =>  45 ( = 5+6+7+8+9+10 )
  *   -1,1  =>  0  ( = -1 + 0 + 1 )
  */
-function getSumBetweenNumbers(/* n1, n2 */) {
-  throw new Error('Not implemented');
+function getSumBetweenNumbers(n1, n2) {
+  return (((n2 - n1) + 1) * (n1 + n2)) / 2;
+  // throw new Error('Not implemented');
 }
 
 
@@ -85,8 +86,11 @@ function getSumBetweenNumbers(/* n1, n2 */) {
  *   10,1,1   =>  false
  *   10,10,10 =>  true
  */
-function isTriangle(/* a, b, c */) {
-  throw new Error('Not implemented');
+function isTriangle(a, b, c) {
+  if ((a + b <= c) || (a + c <= b) || (b + c <= a)) return false;
+  if ((a <= 0) || (b <= 0) || (c <= 0)) return false;
+  return true;
+  // throw new Error('Not implemented');
 }
 
 
